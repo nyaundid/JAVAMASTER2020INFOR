61e3c309979475ec43a0e530483560b8375d69d9
nyaundid <darius.nyaundi@yahoo.com>
2020-10-25 10:06:11 -0500

    STRING JAVA 2

--- java master/string data/src/academy/learnprogramming/Main.java
+++ java master/string data/src/academy/learnprogramming/Main.java
@@ -9,6 +9,17 @@ public class Main {
 	System.out.println("myString is equal to " + myString);
 	myString = myString + "\u00A9 2019";
 	System.out.println("myString is equal to " + myString);
+	String numberString = "250.55";
+	numberString = numberString + "49.95";
+	System.out.println(numberString);
+	String lastString = "10";
+	int myInt = 50;
+	lastString = lastString + myInt;
+	System.out.println("LastString is equal to " + lastString);
+	double doubleNumber = 120.47d;
+	lastString = lastString + doubleNumber;
+	System.out.println("LastString is equal to " + lastString);
+
 
 	}
 }
