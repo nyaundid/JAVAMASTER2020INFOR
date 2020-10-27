a025ee00e84ee9a7fcf8cfb6a6201a59a85d0d10
nyaundid <darius.nyaundi@yahoo.com>
2020-10-26 21:48:52 -0500

    keywords part 2

--- java master/keyword/src/academy/learnprogramming/Main.java
+++ java master/keyword/src/academy/learnprogramming/Main.java
@@ -3,12 +3,22 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	    double kilometres = (100 * 1.609344);
-	    int highScore = 50;
-
-	    if(highScore == 50){
-	        System.out.println("This is an expression");
+        int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
         }
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                "another" +
+                " still more.");
+
+        int anotherVarible = 50;
+        myVariable--;
+        System.out.println("This is another one");
+
 
     }
 }
