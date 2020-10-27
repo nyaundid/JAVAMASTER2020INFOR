f422ddb14fcbefc1b8a8ab8188c4532a50ef6577
nyaundid <darius.nyaundi@yahoo.com>
2020-10-26 22:32:59 -0500

    keywords part working with 2 outcomes

--- java master/keyword/src/academy/learnprogramming/Main.java
+++ java master/keyword/src/academy/learnprogramming/Main.java
@@ -3,22 +3,42 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        int myVariable = 50;
-        if (myVariable == 50) {
-            System.out.println("Printed");
-        }
-        myVariable++;
-        myVariable--;
-        System.out.println("This is a test");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        System.out.println("This is" +
-                "another" +
-                " still more.");
+//        if(score <5000 && score >1000) {
+//			System.out.println("Your score was less than 5000 but greater than 1000");
+//		}else if (score < 1000){
+//        	System.out.println("Your score was less than 5000");
+//
+//		} else {
+//        	System.out.println("Got here");
+//		}
+		if (gameOver == true) {
+			int finalScore = score + (levelCompleted * bonus);
+			finalScore += 1000;
+			System.out.println("Your final score was" + finalScore);
+		}
 
-        int anotherVarible = 50;
-        myVariable--;
-        System.out.println("This is another one");
-
-
+//		int savedFinalScore = finalScore;
+//
+//		boolean newGameOver = true;
+//		int newscore = 1000;
+//		int newlevelCompleted = 8;
+//		int newbonus = 200;
+//
+//		if (newGameOver == true) {
+//			int finalScore = newscore + (newlevelCompleted * newbonus);
+//			System.out.println("Your final score was" + finalScore);
+//		}
+		score = 1000;
+		levelCompleted = 8;
+		bonus = 200;
+		if (gameOver == true) {
+			int finalScore = score + (levelCompleted * bonus);
+			System.out.println("Your final score was" + finalScore);
+		}
     }
 }
